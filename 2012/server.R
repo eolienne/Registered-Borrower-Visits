f29803a9288f7cf5b722014c37b660215ae8233f
Alyssa Hislop <alyssa.l.hislop@gmail.com>
2015-09-16 18:11:57 -0700

    Initial commit

--- 2012/server.R
+++ 2012/server.R
@@ -1,7 +1,33 @@
-#This will be the server file for the % borrowers per state over time
+#Using Ari Lamstein's server-final.R as a base
+
+library(shiny)
+library(choroplethr)
+
 shinyServer(function(input, output) {
   
-  # You can access the value of the widget with input$select, e.g.
-  output$value <- renderPrint({ input$select })
+  output$stateMap = renderPlot({
+    # add a progress bar
+    progress = shiny::Progress$new()
+    on.exit(progress$close())
+    progress$set(message = "Creating image. Please wait.", value = 0)
+    
+    year                         = as.numeric(input$year)
+    df_state_demographics        = get_state_demographics(year, 5)
+    df_state_demographics$value  = df_state_demographics[, input$demographic]
+    state_choropleth(df_state_demographics, num_colors = input$num_colors)
+  })
+  
+  output$countyMap = renderPlot({
+    # add a progress bar
+    progress = shiny::Progress$new()
+    on.exit(progress$close())
+    progress$set(message = "Creating image. Please wait.", value = 0)
+    
+    year                          = as.numeric(input$year)
+    df_county_demographics        = get_county_demographics(year, 5)
+    df_county_demographics$value  = df_county_demographics[, input$demographic]
+    county_choropleth(df_county_demographics, num_colors = input$num_colors)
+  })
+  
   
 })
--- 2012/ui.R
+++ 2012/ui.R
@@ -1,18 +1,32 @@
-shinyUI(fluidPage (
-  titlePanel("Percentage of state population as registered library borrowers"),
-  div(HTML("By <a href='http://wwww.twitter.com/alyssa_briggs'>Alyssa Hislop</a>. Source code here.")),
-  
-  sidebarLayout(position="right"),
-   sidebarPanel("sidebar panel"),
-   mainPanel("main panel"),
+#Using Ari Lamstein's ui.R as a base
 
-#Creating a dropdown selector to toggle between years of data 
-  selectInput("select", label = h3("Select box"), 
-              choices = list("FY 2012" = 1, "FY 2011" = 2, "FY 2010" = 3), 
-              selected = 1),
-  
-  hr(),
-  fluidRow(column(3, verbatimTextOutput("value")))
-  
-  ))
+library(shiny)
+library(choroplethrMaps)
 
+data(df_state_demographics, package="choroplethr")
+demographic_choices = colnames(df_state_demographics)[2:ncol(df_state_demographics)]
+
+shinyUI(fluidPage(
+  
+  titlePanel("Annual public library registered borrower visits"),
+  div(HTML("By <a href='https://iskati.wordpress.com/'>Alyssa Hislop</a>. Source code <a href='https://github.com/eolienne/Registered-Borrower-Visits'>here</a>.")),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId  = "year", 
+                  label    = "Year",
+                  choices  = 2011:2012,
+                  selected = 2012),
+      
+      sliderInput(inputId = "num_colors",
+                  label   = "Select number of colors",
+                  min     = 1,
+                  max     = 9,
+                  value   = 7)),
+    
+    mainPanel(
+      plotOutput("stateMap"),
+      plotOutput("countyMap")
+    )
+  )
+))
